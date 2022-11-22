cd88e6a9d76601a3457e762ec23c460d80ba85d2
Mario <marioriascos1201@gmail.com>
2022-11-22 16:45:28 -0500

    Última Clase

--- CursoPOOUber/PHP/index.php
+++ CursoPOOUber/PHP/index.php
@@ -1,3 +1,8 @@
 <?php
+require_once("Car.php");
+require_once("UberX.php");
+require_once("Account.php");
 
+$uberX = new UberX("CVB123",new Account("Andres Herrera","123456"),"Chevrolet","Spark");
+$uberX->printDataCAr();
 ?>
